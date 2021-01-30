b676eb5fd4a89c53157a3a77c8f6945c29e1139a
marcelosoliveira <msbobsk8@gmail.com>
2021-01-30 18:48:44 -0300

    app-receitas-service

---
+++ precache-manifest.685daef9418e2d9ae323290d99ec2f26.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf68cc39478b7ae439780213d568eb31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ccc687c0c087be61a3",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "9649aa75b69cda52fc3b",
+    "url": "/static/css/main.1101f383.chunk.css"
+  },
+  {
+    "revision": "11ccc687c0c087be61a3",
+    "url": "/static/js/2.87ad4dcd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.87ad4dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9649aa75b69cda52fc3b",
+    "url": "/static/js/main.ddf98517.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  }
+]);
