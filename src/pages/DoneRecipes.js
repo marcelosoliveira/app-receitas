9d607b126a37965872dc06474592787f4a6d896e
marcelosoliveira <msbobsk8@gmail.com>
2021-01-31 23:56:05 -0300

    testando o heroku

--- src/pages/DoneRecipes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Header from '../components/Header';
-
-function DoneRecipes() {
-  return (
-    <Header title="Receitas Feitas" isSearchable={ false } />
-  );
-}
-export default DoneRecipes;
